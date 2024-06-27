2392813fda752a80cc1fc79aff3be57a3e14b42b
PaolaRoMa <92270132+PaolaRoMa@users.noreply.github.com>
2024-06-27 14:56:33 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './Logo.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <img src={logo} alt="Logo de PetTracer" className="logo" />
+      <title>PetTrace</title>
+      <nav className="navbar">
+          <ul className="nav-links">
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/about">Acerca de nosotros</a></li>
+            <li><a href="/contact">Contactanos</a></li>
+            <li><a href="/contact">Regístrate</a></li>
+            <li><a href="/contact">Refugios</a></li>
+            {/* Agrega más elementos según necesites */}
+          </ul>
+        </nav>
       </header>
+      <main>
+        {/* Contenido principal de la página */}
+        <h1>Bienvenido a PetTrace</h1>
+        <p>¡Si necesitas encontrar al dueño de un perrito perdido o si tu mascota se perdió, estas en el lugar correcto!</p>
+      </main>
     </div>
   );
 }
